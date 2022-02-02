81eb1522fd770061b7c9ca781a9997067b567818
colani-sudo <matsenjwacolani@gmail.com>
2022-02-03 05:38:05 +0800

    fourth commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ class Hello{
 
     public static void main(String[] args){
         System.out.println("Running Java in vscode!");
+        int a = 1;
+        int b = 3;
+        if(a + b == 5)
+            System.out.println("Equal");
         System.out.println("Testing pull command in git");
     }
 }
