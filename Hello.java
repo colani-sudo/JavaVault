ad6ad618adb23cb6e475ff889095fc0cd50276c4
Colani <magebac628@gmail.com>
2022-02-03 04:46:36 +0800

    second commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello{
 
     public static void main(String[] args){
         System.out.println("Running Java in vscode!");
+        System.out.println("Testing pull command in git");
     }
 }
