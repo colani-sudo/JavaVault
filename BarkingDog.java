d52398788324fb8ede62236c2eeca47818c57105
colani-sudo <matsenjwacolani@gmail.com>
2022-02-03 21:56:58 +0800

    1st commit

---
+++ BarkingDog.java
@@ -0,0 +1,18 @@
+
+public class BarkingDog {
+    
+    public static void main(String[] args)
+    {
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, 8));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay)
+    {
+        if(barking && hourOfDay >= 0 && hourOfDay < 8 || hourOfDay > 22 && hourOfDay <= 23)
+        {
+          return true;
+        }
+        return false;
+    }
+}
