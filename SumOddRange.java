b3abffae74fe8b165a0bbaa9201dc2006cc9bac7
colani-sudo <matsenjwacolani@gmail.com>
2022-02-07 01:28:50 +0800

    1st commit

---
+++ SumOddRange.java
@@ -0,0 +1,27 @@
+public class SumOddRange {
+ 
+    public static boolean isOdd(int number){
+        
+        if(number > 0){
+            if(number % 2 == 0){
+                return false;
+            }
+            return true;
+        }
+        return false;
+        
+    }
+    public static int sumOdd(int start, int end){
+        
+        int sum = 0, i;
+        if((start < 1 || end < 1) || (end < start)){
+            return -1;
+        }
+            
+        for(i=start; i <= end; i++){
+            if(isOdd(i))
+                sum+=i;
+        }
+        return sum;
+    }
+}
