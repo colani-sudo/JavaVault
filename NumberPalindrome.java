a44956d07b6e87886630c888c85abd24f3695719
colani-sudo <matsenjwacolani@gmail.com>
2022-02-09 16:00:06 +0800

    first commit

---
+++ NumberPalindrome.java
@@ -0,0 +1,19 @@
+public class NumberPalindrome {
+ 
+    public static boolean isPalindrome(int number){
+        
+        int reverse = 0, remainder, digit;
+        number = Math.abs(number);
+        digit = number;
+        while(number > 0){
+            remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number /= 10;
+        }
+       if(digit == reverse) {
+           return true;
+       }else{
+           return false;
+       }
+    }
+   }
