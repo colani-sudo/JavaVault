52e2e817af73931c1a8f86596b0b854820a34e19
colani-sudo <matsenjwacolani@gmail.com>
2022-02-09 15:01:49 +0800

    second commit

--- DigitSum.java
+++ DigitSum.java
@@ -11,10 +11,10 @@ public class DigitSum{
         }
         else{
             while(number >= 10){
-                sum += number % 10;
-                number = number / 10;
+                sum += number % 10; //extract the least significant digit and add it to sum
+                number = number / 10;   //discard the least significant digit
             }
-            sum += number;
+            sum += number;      //add the last significant digit
             return sum;
         }
     }
