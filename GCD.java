578dc91c1872d40a9d2ddbac8860c3da2b1132ef
colani-sudo <matsenjwacolani@gmail.com>
2022-02-11 13:20:04 +0800

    1st commit

---
+++ GCD.java
@@ -0,0 +1,20 @@
+public class GCD{
+    // write your code here
+
+    public static void main(String[] args){
+        System.out.println(getGreatestCommonDivisor(20, 15));
+    }
+    public static int getGreatestCommonDivisor(int x, int y){
+    
+        if(x < 10 || y < 10)
+            return -1;
+        int gcd = 0;
+        for(int i = 1; i <= x && i <= y; i++){
+            if(x%i == 0 && y%i == 0){
+                gcd = i;
+            }
+        }
+    
+    return gcd;
+    }
+}
