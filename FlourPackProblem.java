23ff0fc302940ed04ee5eea127af44a1d0060098
colani-sudo <matsenjwacolani@gmail.com>
2022-03-02 21:01:09 +0800

    final solution

--- FlourPackProblem.java
+++ FlourPackProblem.java
@@ -1,23 +1,32 @@
 public class FlourPackProblem {
     
     public static void main(String[] args){
-        System.out.println(canPack(1,0,6));
+        System.out.println(canPack(1,0,5));
     }
     public static boolean canPack(int bigCount,int smallCount,int goal){
         if(bigCount < 0 || smallCount < 0 || goal < 0)
             return false;
-        int sum = (bigCount*5) + smallCount;
-        if(sum > goal){
-            if(bigCount > 0 && smallCount > 0){
-                return true;
-            }else if(bigCount == 0 && smallCount > 0){
-                return true;
+        int totBig = bigCount * 5;
+        int rem = 0;
+            
+        int totalFlour = totBig + smallCount;
+            
+        if(goal <= totalFlour){
+            int dividentBig = goal / 5;
+            if(dividentBig >= bigCount){
+                rem = goal - totBig;
+            } else {
+                rem = goal - (5 * dividentBig);
             }
-            else{
+            
+            if(rem == 0 || rem <= smallCount){
+                return true;
+            } else {
                 return false;
             }
-        }else{
-            return true;
+            
+        } else {    
+            return false;
         }
     }
 }
