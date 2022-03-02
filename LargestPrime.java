a20fb02b61a99cbc936bdd61da58ce902729b778
colani-sudo <matsenjwacolani@gmail.com>
2022-03-02 22:03:25 +0800

    final solution

---
+++ LargestPrime.java
@@ -0,0 +1,27 @@
+public class LargestPrime {
+
+    public static void main(String[] args){
+        System.out.println(getLargestPrime(21));
+    }
+    
+    public static int getLargestPrime(int number){
+        
+        if(number <= 1){
+            return -1;
+        }
+        
+        int largest = 0;
+        for(int i=1; i <= number; i++){
+            if(number%i == 0){
+                int counter=0;
+                for(int j=1; j < i; j++){
+                    if(i%j == 0)
+                        counter++;
+                }
+                if(counter == 1)
+                    largest = i;
+            }
+        }
+        return largest;
+    }
+}
