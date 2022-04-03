c5108db7e7d11d6ed2e972bb503f0bcdb8391bdb
colani-sudo <matsenjwacolani@gmail.com>
2022-04-03 23:15:41 +0800

    ReadingUserInput.java

--- ReadingUserInput.java
+++ ReadingUserInput.java
@@ -4,16 +4,28 @@ public class ReadingUserInput {
     
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int counter = 1;
-        System.out.println("Enter number #"+counter);
-        boolean hasNextInt = scanner.hasNextInt();
-        while(hasNextInt){
-            int number = scanner.nextInt();
-            //scanner.nextInt();
-            counter++;
-            System.out.println("Enter number #"+counter);
+
+        int counter = 0;
+        int sum = 0;
+        
+        while(true){
+            int order = counter + 1;
+            System.out.println("Enter number #"+ order + ":");
+            boolean isAnInt = scanner.hasNextInt();
+            if(isAnInt){
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                if(counter == 10)       //can use the while loop to terminate 
+                    break;
+            }else{
+                System.out.println("Invalid number");
+            }
+
+            scanner.nextLine(); //to handle end of line (enter key)
         }
-        System.out.println("Invalid number #");
+        System.out.println("Sum = " + sum);
+        scanner.close();
 
     }
 }
